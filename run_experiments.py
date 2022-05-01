430b9cbd6f61941147d1d095de13b7fab7787b21
Ameya Shere <ameya.shere@publicissapient.com>
2022-05-01 06:14:12 -0400

    uncomment

--- run_experiments.py
+++ run_experiments.py
@@ -18,37 +18,37 @@ gamma = np.sqrt(5)
 
 ci = 90
 
-# # TEST 1: Simulated Gaussian data
-# print("TEST 1: Simulated Gaussian data")
+# TEST 1: Simulated Gaussian data
+print("TEST 1: Simulated Gaussian data")
 
-# # Simulate Gaussian data
-# X_train, X_test, y_train, y_test = simulate_gaussian(ratio, n, p, gamma)
+# Simulate Gaussian data
+X_train, X_test, y_train, y_test = simulate_gaussian(ratio, n, p, gamma)
 
-# # Test with baseline model
-# p_vals_baseline, pred_ints_baseline = test_baseline(X_train.numpy(), y_train.numpy(), X_test.numpy(), ci=ci)
+# Test with baseline model
+p_vals_baseline, pred_ints_baseline = test_baseline(X_train.numpy(), y_train.numpy(), X_test.numpy(), ci=ci)
 
-# # Test with SLOE Model
-# p_vals_sloe, pred_ints_sloe = test_sloe(X_train.numpy(), y_train.numpy(), X_test.numpy(), ci=ci)
+# Test with SLOE Model
+p_vals_sloe, pred_ints_sloe = test_sloe(X_train.numpy(), y_train.numpy(), X_test.numpy(), ci=ci)
 
-# # Generate plots
-# plot_p_vals(p_vals_baseline, p_vals_sloe, 'Simulated Gaussian', 'gaussian')
-# plot_conf_ints(pred_ints_baseline, pred_ints_sloe, X_test.numpy(), y_test.numpy(), 'Simulated Gaussian', 'gaussian', ci=ci)
+# Generate plots
+plot_p_vals(p_vals_baseline, p_vals_sloe, 'Simulated Gaussian', 'gaussian')
+plot_conf_ints(pred_ints_baseline, pred_ints_sloe, X_test.numpy(), y_test.numpy(), 'Simulated Gaussian', 'gaussian', ci=ci)
 
-# # TEST 2: Simulated Non-Gaussian data
-# print('TEST 2: Simulated Non-Gaussian data')
+# TEST 2: Simulated Non-Gaussian data
+print('TEST 2: Simulated Non-Gaussian data')
 
-# # Simulate Non-Gaussian data with normalizing flow
-# X_train_flow, X_test_flow = normalizing_flow(X_train, X_test)
+# Simulate Non-Gaussian data with normalizing flow
+X_train_flow, X_test_flow = normalizing_flow(X_train, X_test)
 
-# # Test with baseline model
-# p_vals_baseline, pred_ints_baseline = test_baseline(X_train_flow.numpy(), y_train.numpy(), X_test_flow.numpy(), ci=ci)
+# Test with baseline model
+p_vals_baseline, pred_ints_baseline = test_baseline(X_train_flow.numpy(), y_train.numpy(), X_test_flow.numpy(), ci=ci)
 
-# # Test with SLOE Model
-# p_vals_sloe, pred_ints_sloe = test_sloe(X_train_flow.numpy(), y_train.numpy(), X_test_flow.numpy(), ci=ci)
+# Test with SLOE Model
+p_vals_sloe, pred_ints_sloe = test_sloe(X_train_flow.numpy(), y_train.numpy(), X_test_flow.numpy(), ci=ci)
 
-# # Generate plots
-# plot_p_vals(p_vals_baseline, p_vals_sloe, 'Simulated Non-Gaussian', 'non_gaussian')
-# plot_conf_ints(pred_ints_baseline, pred_ints_sloe, X_test_flow.numpy(), y_test.numpy(), 'Simulated Non-Gaussian', 'non_gaussian', ci=ci)
+# Generate plots
+plot_p_vals(p_vals_baseline, p_vals_sloe, 'Simulated Non-Gaussian', 'non_gaussian')
+plot_conf_ints(pred_ints_baseline, pred_ints_sloe, X_test_flow.numpy(), y_test.numpy(), 'Simulated Non-Gaussian', 'non_gaussian', ci=ci)
 
 # TEST 3: Latent Variables
 print('TEST 3: Latent Variables')
